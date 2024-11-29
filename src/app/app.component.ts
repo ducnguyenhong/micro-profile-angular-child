9b39ae98ce7c36dbbad912dd01e5e4af0160f1c7
Nguyen Hong Duc <autoclickvn@gmail.com>
2024-11-29 08:35:44 +0700

    Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-child';
+}
+
+export default AppComponent;
